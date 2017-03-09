0d99213257504f2fd781f79a60a05fd7da645e09
Caevol <logan@loganpedersen.com>
2017-03-08 21:22:56 -0700

    Added Saved_Frontier functionality, puzzle now solves 5x5 in reasonable time

--- SlidePuzzleSolver.java
+++ SlidePuzzleSolver.java
@@ -5,8 +5,13 @@ import java.util.*;
  */
 public class SlidePuzzleSolver {
 
-    //resets the priority_queue after reaching a step that is a multiple of RESET_RATE, keep this smaller than 30 and greater than 10 for good results.
-    final int RESET_RATE = 20;
+    //resets the priority_queue after reaching a step that is a multiple of RESET_RATE, keep this smaller than 30
+    //as the puzzle size gets bigger, consider making the reset_rate smaller
+    final int RESET_RATE = 7;
+    final int SAVED_FRONTIER = 25;
+
+    private int stepsTaken = 0;
+    private int resets = 0;
     //heuristic used to compare map states
     Comparator<MapState> compareMaps = (MapState o1, MapState o2)->
             new Integer(o1.getDistance()+o1.getStepNum()).compareTo(o2.getDistance()+o2.getStepNum());
@@ -20,23 +25,31 @@ public class SlidePuzzleSolver {
     public SlidePuzzleSolver(MapState root){
         puzzleQueue = new PriorityQueue<MapState>(compareMaps);
         pointerMap = root;
-        int stepsTaken = 0;
-        int resets = 0;
         while(!pointerMap.isSolved()){
             if(pointerMap.getStepNum() - stepsTaken > RESET_RATE){
-                resets ++;
-                stepsTaken = pointerMap.getStepNum();
-                System.out.println("Reset priority Queue : " + resets);
-                puzzleQueue.clear();
+                resetQueue();
+                System.out.println("Distance: " + pointerMap.getDistance());
             }
             addMapCasesToQueue();
             pointerMap = puzzleQueue.poll();
         }
-
+        pointerMap.printSolution();
         System.out.println("Puzzle solved in : " + pointerMap.getStepNum() + " steps!");
 
-        pointerMap.printSolution();
+    }
 
+    private void resetQueue(){
+        resets ++;
+        stepsTaken = pointerMap.getStepNum();
+        System.out.println("Reset priority Queue : " + resets);
+        Stack<MapState> saveList = new Stack<MapState>();
+        for(int i = 0; i < SAVED_FRONTIER; ++i){
+            saveList.add(puzzleQueue.poll());
+        }
+        puzzleQueue.clear();
+        for(int i = 0; i < SAVED_FRONTIER; ++i){
+            puzzleQueue.add(saveList.pop());
+        }
     }
 
     private void addMapCasesToQueue(){
@@ -103,21 +116,25 @@ public class SlidePuzzleSolver {
     }
 
     public static void main(String[] args) {
+       /*
+        4x4 puzzle
         ArrayList<Integer> a_1 = new ArrayList<Integer>(Arrays.asList(13, 0, 2, 3));
         ArrayList<Integer> a_2 = new ArrayList<Integer>(Arrays.asList(1, 15, 12, 6));
         ArrayList<Integer> a_3 = new ArrayList<Integer>(Arrays.asList(8, 4, 9, 5));
         ArrayList<Integer> a_4 = new ArrayList<Integer>(Arrays.asList(10, 7, 14, 11));
 
         ArrayList<ArrayList<Integer>> l = new ArrayList<>(Arrays.asList(a_1, a_2, a_3, a_4));
-        MapState rootMap = new MapState(l);
+        */
 
-        //print map before beginning
-        for(ArrayList<Integer> a : rootMap.getMapState()){
-            for(Integer i : a){
-                System.out.print(i.toString() + ' ');
-            }
-            System.out.print('\n');
-        }
+        // 5x5 puzzle
+        ArrayList<Integer> a_1 = new ArrayList<Integer>(Arrays.asList(5,22,18,24,14));
+        ArrayList<Integer> a_2 = new ArrayList<Integer>(Arrays.asList(1,3,11,2,13));
+        ArrayList<Integer> a_3 = new ArrayList<Integer>(Arrays.asList(7,0,21,4,9));
+        ArrayList<Integer> a_4 = new ArrayList<Integer>(Arrays.asList(10,6,15,17,12));
+        ArrayList<Integer> a_5 = new ArrayList<Integer>(Arrays.asList(20,8,19,16,23));
+
+        ArrayList<ArrayList<Integer>> l = new ArrayList<>(Arrays.asList(a_1, a_2, a_3, a_4, a_5));
+        MapState rootMap = new MapState(l);
 
         SlidePuzzleSolver s = new SlidePuzzleSolver(rootMap);
     }
